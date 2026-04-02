da31163770086d368a4987f9357b14a0e4b2086f
chandra <icang@chandras-MacBook-Pro.local>
2026-04-02 17:37:41 +0700

    v16-v17 di sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // ===== SERVICE WORKER — CatatDuit v2 =====
-const CACHE_NAME = "catatduit-v2-cache-v16";
+const CACHE_NAME = "catatduit-v2-cache-v17";
 
 const ASSETS_TO_CACHE = [
   "/catatduit/",
